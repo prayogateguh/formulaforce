dbe81a52f3ad326bb9ee4416c24161a7658f6380
Prayoga Teguh <teguh.mow@gmail.com>
2023-04-06 06:14:37 +0700

    end of chapter-3

--- scripts/sfdx/dev.sh
+++ scripts/sfdx/dev.sh
@@ -5,3 +5,7 @@ sfdx force:user:permset:assign --perm-set-name FFRaceManagementUpdateDNFStatus
 sfdx force:user:permset:assign --perm-set-name FFRaceManagement
 sfdx force:user:permset:assign --perm-set-name FFRaceManagementAddDriver
 sfdx force:user:permset:assign --perm-set-name FFRaceAnalytics
+# Export Custom Metadata
+sfdx force:cmdt:record:insert --filepath ./data/TrackLapRecords.csv --typename TrackLapRecord__mdt
+# Export sample data
+sfdx data import tree -p ./data/Data-plan.json
